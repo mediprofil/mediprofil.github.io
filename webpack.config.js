4c1fd64e46455021152b964e51368f030307d5e0
Petar Karadzhov <petar@karadzhov.com>
2020-07-18 01:24:04 +0200

    Configure webpack html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,11 @@
-module.exports = (env, argv) => ({})
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = (env, argv) => ({
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html"),
+      hash: argv.mode === 'development'
+    })
+  ]
+})
