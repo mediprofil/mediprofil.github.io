c26d2a325d7fb5c7031f04ab25735caeac040a67
Petar Karadzhov <petar@karadzhov.com>
2020-07-19 23:45:40 +0200

    Prepare the configuration for the deploy task

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,17 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 const path = require("path");
 
 module.exports = (env, argv) => ({
   output: {
-    filename: '[name].[chunkhash].js'
-  },
-  optimization: {
-    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    filename: '[name].[chunkhash].js',
+    publicPath: 'public'
   },
   plugins: [
+    new CleanWebpackPlugin('dist', {} ),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src", "index.html"),
       hash: argv.mode === 'production'
@@ -50,5 +50,19 @@ module.exports = (env, argv) => ({
         ],
       },
     ],
+  },
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    moduleIds: 'hashed',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
   },
 })
